99b3353a105a59f320a22243793fbb3ec4acae6b
David Ruiz de la Rosa <ddlarosa@thesensorylab.es>
2019-01-03 16:21:21 +0100

    Fixe Test with Devise

--- test/controllers/sub_categories_controller_test.rb
+++ test/controllers/sub_categories_controller_test.rb
@@ -1,48 +1,52 @@
 require 'test_helper'
 
 class SubCategoriesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @sub_category = sub_categories(:tom)
+    @sub_category = sub_categories(:one)
+    @user = users(:tom)
+    sign_in @user
   end
 
-  # test "should get index" do
-  #   get category_sub_categories_url(categories(:one))
-  #   assert_response :success
-  # end
-  #
-  # test "should get new" do
-  #   get new_category_sub_category_url(categories(:one))
-  #   assert_response :success
-  # end
-  #
-  # test "should create sub_category" do
-  #   assert_difference('SubCategory.count') do
-  #     post category_sub_categories_url(categories(:one)), params: { sub_category: { category_id: categories(:one).id, description: @sub_category.description, name: "New Subcategory" } }
-  #   end
-  #
-  #   assert_redirected_to categories_url
-  # end
-  #
-  # test "should show sub_category" do
-  #   get category_sub_category_url(categories(:one),@sub_category)
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get edit_category_sub_category_url(categories(:one),@sub_category)
-  #   assert_response :success
-  # end
-  #
-  # test "should update sub_category" do
-  #   patch category_sub_category_url(categories(:one), @sub_category), params: { sub_category: { category_id: @sub_category.category_id, description: @sub_category.description, name: @sub_category.name } }
-  #   assert_redirected_to categories_url
-  # end
-  #
-  # test "should destroy sub_category" do
-  #   assert_difference('SubCategory.count', -1) do
-  #     delete category_sub_category_url(categories(:one), @sub_category)
-  #   end
-  #
-  #   assert_redirected_to category_sub_categories_url(categories(:one))
-  # end
+  test "should get index" do
+    get category_sub_categories_url(categories(:one))
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_sub_category_url(categories(:one))
+    assert_response :success
+  end
+
+  test "should create sub_category" do
+    assert_difference('SubCategory.count') do
+      post category_sub_categories_url(categories(:one)), params: { sub_category: { category_id: categories(:one).id, description: @sub_category.description, name: "New Subcategory" } }
+    end
+
+    assert_redirected_to categories_url
+  end
+
+  test "should show sub_category" do
+    get category_sub_category_url(categories(:one),@sub_category)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_category_sub_category_url(categories(:one),@sub_category)
+    assert_response :success
+  end
+
+  test "should update sub_category" do
+    patch category_sub_category_url(categories(:one), @sub_category), params: { sub_category: { category_id: @sub_category.category_id, description: @sub_category.description, name: @sub_category.name } }
+    assert_redirected_to categories_url
+  end
+
+  test "should destroy sub_category" do
+    assert_difference('SubCategory.count', -1) do
+      delete category_sub_category_url(categories(:one), @sub_category)
+    end
+
+    assert_redirected_to category_sub_categories_url(categories(:one))
+  end
 end
