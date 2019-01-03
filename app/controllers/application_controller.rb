2a0945c4db37a966565f24b0fae3d834254795ef
David Ruiz de la Rosa <ddlarosa@thesensorylab.es>
2019-01-03 16:52:28 +0100

    Custom Login Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   before_action :authenticate_user!
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+
 end
